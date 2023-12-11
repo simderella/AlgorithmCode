d990f18a474d215e378275e0c2124cde7265928e
simderella <146172523+simderella@users.noreply.github.com>
2023-12-11 09:30:15 +0900

    feat : 16 algorithm

---
+++ AlgorithmCodeKata/Class16.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class Class16
+    {
+        public class Solution16
+        {
+            public long[] solution(int x, int n)
+            {
+                long[] answer = new long[n];
+
+                for (int i = 1; i <= n; i++)
+                {
+                    answer[i - 1] = (long)x * i;
+                    // answer[0] = (long)x
+                    // answer[1] = (long)x*2
+                }
+
+                return answer;
+            }
+        }
+    }
+}
